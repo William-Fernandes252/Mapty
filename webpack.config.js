d266b6b2a464386f9844d0ae1f0c1bba60fc58f6
William-Fernandes252 <william.winchester1967@gmail.com>
2022-05-22 23:12:25 -0300

    Update configuration to correctly handle leaflet assets

--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,33 @@ module.exports = {
                 use: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
-                test: /\.(png|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        esModule: false,
+                    },
+                },
             },
         ],
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            leaflet_css: __dirname + '/node_modules/leaflet/dist/leaflet.css',
+            leaflet_marker:
+                __dirname + '/node_modules/leaflet/dist/images/marker-icon.png',
+            leaflet_marker_2x:
+                __dirname +
+                '/node_modules/leaflet/dist/images/marker-icon-2x.png',
+            leaflet_marker_shadow:
+                __dirname +
+                '/node_modules/leaflet/dist/images/marker-shadow.png',
+        },
     },
     output: {
         filename: 'bundle.js',
