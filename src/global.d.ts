1a5b6364f1ab13205481a20275e0bc804160d00c
William-Fernandes252 <william.winchester1967@gmail.com>
2022-05-23 21:51:08 -0300

    Define Mapty namespace

---
+++ src/global.d.ts
@@ -0,0 +1,37 @@
+declare module '*.png' {
+    const value: any;
+    export = value;
+}
+
+declare namespace Mapty {
+    export type coords = [number, number]; /* [lat, lng] */
+
+    export interface IApp {
+        #map: L.Map | undefined;
+        #mapEvent: L.LeafletMouseEvent | undefined;
+        #form: JQuery<HTMLElement>;
+        #inputType: JQuery<HTMLElement>;
+        #inputDistance: JQuery<HTMLElement>;
+        #inputDuration: JQuery<HTMLElement>;
+        #inputCadence: JQuery<HTMLElement>;
+        #inputElevation: JQuery<HTMLElement>;
+        _getPosition: () => void;
+        _loadMap: (position: GeolocationPosition) => void;
+        _showForm: (e: L.LeafletMouseEvent) => void;
+        _toggleElevationField: () => void;
+        _newWorkout: (
+            e: JQuery.SubmitEvent<
+                HTMLElement,
+                undefined,
+                HTMLElement,
+                HTMLElement
+            >
+        ) => void;
+    }
+
+    export interface IWorkout {
+        coords: coords;
+        duration: number;
+        distance: number;
+    }
+}
